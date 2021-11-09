86bb0110f58cbdfed0a3775e8c6d7ba915518975
Paul Lu <luhanfeng1106@sina.com>
2021-11-09 12:13:18 -0600

    update

---
+++ DisjointSetTests.java
@@ -0,0 +1,37 @@
+/* Academic Honesty Certification
+* Written sources used:
+* (Include textbook(s), complete citations for web or other written sources.
+* Note that you are not allowed to use the web for this assignment.
+* Write "none" if no sources used.)
+* ProblemSet5.pdf
+*
+*
+* Help obtained:
+* (Include names of anyone other than the instructor.)
+* NA
+*
+*
+* My written or typed signature below confirms that the above list
+* of sources is complete.
+* Signature: Garry Gao, Paul Lu
+*/
+
+public class DisjointSetTests {
+  static void makeSetTest() {
+
+  }
+
+  static void findSetTest() {
+
+  }
+
+  static void unionTest() {
+
+  }
+
+  public static void main(String[] args) {
+    makeSetTest();
+    findSetTest();
+    unionTest();
+  }
+}
